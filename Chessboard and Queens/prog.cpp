5b38d6d6e866709c4de0f7e7d34cf13768c5e677
pnaskardev <priyanshunaskar89@gmail.com>
2023-03-17 19:43:05 +0530

    my solution of N queen has been added

--- Chessboard and Queens/prog.cpp
+++ Chessboard and Queens/prog.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 #include<bits/stdc++.h>
-#define int long long int
 using namespace std;
 void solve();
-int leftDiagonal[15],rightDiagonal[15],row[7];
 char chess [8][8];
 static int counter=0;
-void place(int index); 
+void place(int i); 
+bool check(int i,int j);
 int32_t main(void)
 {
     int t=1;
@@ -28,24 +27,66 @@ void solve()
     place(0);
     cout<<counter<<endl;
 }
-void place(int j)
+bool check(int i,int j)
 {
-    if(j==8)
+    // check the columns if there is any queen or not
+    for(int k=0;k<i;k++)
     {
+        if(chess[k][j]=='Q')
+        {
+            return false;
+        }
+    }
+
+    // check the left side of the diagonal
+    for(int k=i,l=j;k>=0 && l>=0;k--,l--)
+    {
+        if(chess[k][l]=='Q')
+        {
+            return false;
+        }
+    }
+
+    // check the right side of the diagonal
+    for(int k=i,l=j;k>=0 && l<8;k--,l++)
+    {
+        if(chess[k][l]=='Q')
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
+void place(int i)
+{
+    // if i==8 then we have reached at the end of the board 
+    // and we need to return
+    if(i==8)
+    {
+        // cout<<counter<<endl;
         counter++;
         return;
     }
-    for(int i=0;i<8;i++)
+    for(int j=0;j<8;j++)
     {
-        if(chess[i][j]=='.' && leftDiagonal[i-j+7]==0 && rightDiagonal[i+j]==0 && row[i]==0 )
+        // check if queen is already placed in this position
+        if(chess[i][j]=='*' || !check(i,j))
         {
-            leftDiagonal[i-j+7]=1;
-            rightDiagonal[i+j]=1;
-            row[i]=1;
-            place(j+1);
-            leftDiagonal[i-j+7]=0;
-            rightDiagonal[i+j]=0;
-            row[i]=0;
+            continue;
         }
+        // simce the conditions above are not true
+        // queen can be placed here 
+        chess[i][j]='Q';
+        // after placing the queen 
+        // go to the next row and try to place the queen
+        place(i+1);
+        // no matter what happens if the queen can be placed or not
+        // we should clear the current current position so that
+        // the queen can be moved and other combinations
+        // other combinations can be explored 
+        chess[i][j]='.';
+
+
     }
 }
